1e83a9803220913c16b62303dbc4bc6e8178cb4e
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2023-09-25 14:23:28 -0500

    progress

--- src/component/MapSettings.jsx
+++ src/component/MapSettings.jsx
@@ -1,33 +1,28 @@
 import React from "react";
+import styles from "../styles/mapSettings.css";
 
-const MapSettings = ({
-    handleZipInputChange,
-    zipInput,
-    geoFindMe,
+const MapSettings = ({ handleZipInputChange, zipInput, geoFindMe }) => {
+  const apiKey = import.meta.env.VITE_GOOGLE;
 
-}) => {
-    const apiKey = import.meta.env.VITE_GOOGLE;
-
-    return (
-        <div className="map-settings">
-            <div className="map-settings-buttons">
-                <button className="geo-button" onClick={() => geoFindMe()}>
-                    Find Location
-                </button>
-                {/* <span className="or">or</span> */}
-                <div className="zipcode-input-container">
-                    <input
-                        type="text"
-                        id="zipcode"
-                        value={zipInput}
-                        onChange={handleZipInputChange}
-                        maxLength="5"
-                        placeholder="Enter Zip Code"
-                    />
-                </div>
-            </div>
+  return (
+    <div className="map-settings">
+      <div className="map-settings-buttons">
+        <button className="geo-button" onClick={() => geoFindMe()}>
+          Find Location
+        </button>
+        <div className="zipcode-input-container">
+          <input
+            type="text"
+            id="zipcode"
+            value={zipInput}
+            onChange={handleZipInputChange}
+            maxLength="5"
+            placeholder="Enter Zip Code"
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MapSettings;
