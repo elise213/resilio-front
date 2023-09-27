d2693cb6b9b58780e1cb7d53d0773a0e2437901a
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2023-09-27 09:01:30 -0500

    selection component and map

--- src/component/LoginModal.jsx
+++ src/component/LoginModal.jsx
@@ -3,205 +3,223 @@ import { Context } from "../store/appContext";
 import styles from "../styles/loginModal.css";
 import Swal from "sweetalert2";
 
-const LoginModal = ({ log, setLog, email, setEmail, password, setPassword, name, setName, is_org, setIs_org, handleSelectImage, handleRegister, setOpenLoginModal, handleLogin }) => {
-    const { store, actions } = useContext(Context);
+const LoginModal = ({
+  log,
+  setLog,
+  email,
+  setEmail,
+  password,
+  setPassword,
+  name,
+  setName,
+  is_org,
+  setIs_org,
+  handleSelectImage,
+  handleRegister,
+  setOpenLoginModal,
+  handleLogin,
+}) => {
+  const { store, actions } = useContext(Context);
 
-    console.log("LOG", log)
-    let field = null;
-    if (log == "2") {
-        field = (
-            <div className="login-modal-content">
-                <div className="login-modal-header">
-                    <div className="center-header">
-                        <span className="form-label" id="exampleloginModalLabel">
-                            {/* Please Register */}
-                        </span>
-                    </div>
-                    <button
-                        type="button"
-                        className="btn-close"
-                        onClick={() => setLog("1")}
-                    ></button>
-                </div>
-                <div className="login-modal-body">
-                    <form>
-                        <span className="form-label">Do you represent an organization?</span>
-                        <div className="yes-or-no">
-                            <div className="form-check">
-                                <input
-                                    className="form-check-input radio"
-                                    type="radio"
-                                    name="orgRadio"
-                                    id="orgRadio1"
-                                    value={is_org}
-                                    onChange={() => setIs_org("true")}
-                                />
-                                <label className="form-label radio-label" htmlFor="exampleRadios1">
-                                    Yes
-                                </label>
-                            </div>
-
-                            <div className="form-check">
-                                <input
-                                    className="form-check-input radio"
-                                    type="radio"
-                                    name="orgRadio"
-                                    id="orgRadio2"
-                                    value={is_org}
-                                    onChange={() => setIs_org("false")}
-                                />
-                                <label className="form-label radio-label" htmlFor="exampleRadios1">
-                                    No
-                                </label>
-                            </div>
-                        </div>
-                        <div className="">
-                            <label htmlFor="name" className="form-label less-margin">
-                                Name
-                            </label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="name"
-                                aria-describedby="emailHelp"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                            ></input>
-                        </div>
-                        <div className="">
-                            <label htmlFor="email1" className="form-label">
-                                Email
-                            </label>
-                            <input
-                                type="email"
-                                className="form-control"
-                                id="email"
-                                aria-describedby="emailHelp"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            ></input>
-                        </div>
-                        <div className="">
-                            <label htmlFor="password" className="form-label">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                className="form-control"
-                                id="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            ></input>
-                        </div>
-                        <p className="form-label">Choose your avatar</p>
-                        <div className="avatar-div">
-                            {store.avatarImages.map((i, idx) => {
-                                return (
-                                    <span
-                                        key={i}
-                                        className={`${i} avatarImages`}
-                                        id={"avatar" + idx}
-                                        onClick={() => handleSelectImage(idx)}
-                                    />
-                                );
-                            })}
-                        </div>
-
-                        <button
-                            type="submit"
-                            className="submit"
-                            data-bs-dismiss="loginModal"
-                            style={{ width: "100%" }}
-                            onClick={(e) => handleRegister(e)}
-                        >
-                            Register
-                        </button>
-                        <div className="login-modal-footer" >
-                            <div className="forgot-password" onClick={() => setLog("1")}>
-                                Go back to login
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        );
-    } if (log == "1") {
-        field = (
-            <div className="login-modal-content">
-                <div className="login-modal-header">
-                    <div className="header-div">
-                        <span className="form-label" id="">
-                            {/* Please Login */}
-                        </span>
-                    </div>
-                    <button
-                        type="button"
-                        className="btn-close"
-                        data-bs-dismiss="loginModal"
-                        aria-label="Close"
-                        onClick={() => setOpenLoginModal(false)}
-                    ></button>
-                </div>
-                <div className="login-modal-body">
-                    <form>
-                        <div className="">
-                            <label htmlFor="exampleInputEmail1" className="form-label">
-                                Email address
-                            </label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="exampleInputEmail1"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            ></input>
-                        </div>
-                        <div className="">
-                            <label htmlFor="exampleInputPassword1" className="form-label">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                className="form-control"
-                                id="exampleInputPassword1"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            ></input>
-                        </div>
-
-                        <div style={{ "width": "100%" }}>
-                            <button
-                                type="submit"
-                                className="submit"
-                                onClick={(e) => handleLogin(e)}
-                            >
-                                Submit
-                            </button>
-                        </div>
-
-                    </form>
-                </div>
-                <div className="login-modal-footer">
-                    <div
-                        className="forgot-password"
-                        onClick={() => setLog("2")}
-                    >
-                        Register for an account
-                    </div>
-                    <div className="forgot-password"
-                        onClick={() => setLog("3")}>
-                        I forgot my password
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
-    return (
-        <div className="centered">
-            {field}
+  console.log("LOG", log);
+  let field = null;
+  if (log == "2") {
+    field = (
+      <div className="login-modal-content">
+        <div className="login-modal-header">
+          <div className="center-header">
+            <span className="form-label" id="exampleloginModalLabel">
+              {/* Please Register */}
+            </span>
+          </div>
+          <button
+            type="button"
+            className="btn-close"
+            onClick={() => setLog("1")}
+          ></button>
         </div>
-    )
-}
+        <div className="login-modal-body">
+          <form>
+            <span className="form-label">
+              Do you represent an organization?
+            </span>
+            <div className="yes-or-no">
+              <div className="form-check">
+                <input
+                  className="form-check-input radio"
+                  type="radio"
+                  name="orgRadio"
+                  id="orgRadio1"
+                  value={is_org}
+                  onChange={() => setIs_org("true")}
+                />
+                <label
+                  className="form-label radio-label"
+                  htmlFor="exampleRadios1"
+                >
+                  Yes
+                </label>
+              </div>
 
-export default LoginModal
+              <div className="form-check">
+                <input
+                  className="form-check-input radio"
+                  type="radio"
+                  name="orgRadio"
+                  id="orgRadio2"
+                  value={is_org}
+                  onChange={() => setIs_org("false")}
+                />
+                <label
+                  className="form-label radio-label"
+                  htmlFor="exampleRadios1"
+                >
+                  No
+                </label>
+              </div>
+            </div>
+            <div className="">
+              <label htmlFor="name" className="form-label less-margin">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                aria-describedby="emailHelp"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></input>
+            </div>
+            <div className="">
+              <label htmlFor="email1" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                aria-describedby="emailHelp"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+            </div>
+            <div className="">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
+            <p className="form-label">Choose your avatar</p>
+            <div className="avatar-div">
+              {store.avatarImages.map((i, idx) => {
+                return (
+                  <span
+                    key={i}
+                    className={`${i} avatarImages`}
+                    id={"avatar" + idx}
+                    onClick={() => handleSelectImage(idx)}
+                  />
+                );
+              })}
+            </div>
+
+            <button
+              type="submit"
+              className="submit"
+              data-bs-dismiss="loginModal"
+              style={{ width: "100%" }}
+              onClick={(e) => handleRegister(e)}
+            >
+              Register
+            </button>
+            <div className="login-modal-footer">
+              <div className="forgot-password" onClick={() => setLog("1")}>
+                Go back to login
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+  if (log == "1") {
+    field = (
+      <div className="login-modal-content">
+        <div className="login-modal-header">
+          <div className="header-div">
+            <span className="form-label" id="">
+              {/* Please Login */}
+            </span>
+          </div>
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="loginModal"
+            aria-label="Close"
+            onClick={() => setOpenLoginModal(false)}
+          ></button>
+        </div>
+        <div className="login-modal-body">
+          <form>
+            <div className="">
+              <label
+                htmlFor="exampleInputEmail1"
+                className="form-label margin-cancel"
+              >
+                Email address
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputEmail1"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+            </div>
+            <div className="">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
+
+            <div style={{ width: "100%" }}>
+              <button
+                type="submit"
+                className="submit"
+                onClick={(e) => handleLogin(e)}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="login-modal-footer">
+          <div className="forgot-password" onClick={() => setLog("2")}>
+            Register for an account
+          </div>
+          <div className="forgot-password" onClick={() => setLog("3")}>
+            I forgot my password
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return <div className="centered">{field}</div>;
+};
+
+export default LoginModal;
---
+++ src/component/ModalMap.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import GoogleMapReact from "google-map-react";
+import { useNavigate } from "react-router-dom";
+import Styles from "../styles/simpleMap.css";
+
+export const ModalMap = (props) => {
+  let lat = parseFloat(props.latitude);
+  let lng = parseFloat(props.longitude);
+
+  const [city, setCity] = useState({
+    center: { lat: lat, lng: lng },
+  });
+
+  const Marker = ({ lat, lng, color, text, category }) => {
+    const [isHovered, setIsHovered] = useState(false);
+    const navigate = useNavigate(); //
+    const handleMouseEnter = () => {
+      setIsHovered(true);
+    };
+    const handleMouseLeave = () => {
+      setIsHovered(false);
+    };
+
+    const handleMarkerClick = () => {
+      const wantDirections = window.confirm(
+        "Do you want directions to this resource?"
+      );
+      if (wantDirections) {
+        const url = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`;
+        window.open(url, "_blank");
+      }
+    };
+    return (
+      <div
+        className="marker"
+        style={{ color: color, cursor: "pointer" }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onClick={handleMarkerClick}
+      >
+        <div className="marker-icon">
+          <i className="fa-solid fa-map-pin"></i>
+
+          {isHovered && text && <span>Click for Directions</span>}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div className="map-container" style={{ height: "40vh", width: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyDOhqYOYIXvrk8lt2HQQLI8cS1O8FnZt9I" }}
+          center={city.center}
+          defaultZoom={14}
+        >
+          <Marker lat={lat} lng={lng} color="red" text="hi" />
+        </GoogleMapReact>
+      </div>
+    </div>
+  );
+};
