e445627a3bc8aae03d76127ae3827415655dc1c6
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2023-10-13 14:55:05 -0500

    progress on redesign

--- src/component/SimpleMap.jsx
+++ src/component/SimpleMap.jsx
@@ -1,10 +1,30 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
+import Selection from "./Selection";
+import ErrorBoundary from "./ErrorBoundary";
+import MapSettings from "./MapSettings";
+import Report from "./Report";
 import GoogleMapReact from "google-map-react";
 import Styles from "../styles/simple_map.css";
-import MapSettings from "./MapSettings";
+// import MapSettings from "./MapSettings";
 
-const SimpleMap = ({ openModal, handleBoundsChange, city }) => {
+const SimpleMap = ({
+  openModal,
+  handleBoundsChange,
+  INITIAL_DAY_STATE,
+  searchingToday,
+  setSearchingToday,
+  setCategories,
+  setGroups,
+  setDays,
+  city,
+  geoFindMe,
+  handleZipInputChange,
+  zipInput,
+  categories,
+  days,
+  groups,
+}) => {
   const apiKey = import.meta.env.VITE_GOOGLE;
   const { store, actions } = useContext(Context);
 
@@ -77,7 +97,7 @@ const SimpleMap = ({ openModal, handleBoundsChange, city }) => {
 
     const icons = categories.map((category, index) => {
       const iconClass = actions.getIconForCategory(category);
-      const color = actions.getColorForCategory(category).color; // Updated to get color for each category
+      const color = actions.getColorForCategory(category).color;
       return <i key={index} className={iconClass} style={{ color }}></i>;
     });
 
@@ -98,89 +118,86 @@ const SimpleMap = ({ openModal, handleBoundsChange, city }) => {
           {isHovered && text && (
             <span className="marker-text">
               {text}
-              <span className="marker-hover-icons">
-                {icons} {/* Place icons next to the text */}
-              </span>
+              {icons}
             </span>
           )}
         </div>
       </div>
     );
-
-    // return (
-    //   <div
-    //     className="marker"
-    //     style={{ cursor: "pointer" }}
-    //     onMouseEnter={() => setIsHovered(true)}
-    //     onMouseLeave={() => setIsHovered(false)}
-    //     onClick={() => openModal(result)}
-    //   >
-    //     <div className="marker-icon">
-    //       <i className={iconClass} style={{ color }}></i>
-    //       {isHovered && (
-    //         <span className="marker-hover-icons">
-    //           {icons} {/* Updated this to directly insert the icons elements */}
-    //         </span>
-    //       )}
-    //       {isHovered && text && (
-    //         <span className="marker-text">
-    //           {text} {/* Removed icons from here */}
-    //         </span>
-    //       )}
-    //     </div>
-    //   </div>
-    // );
   };
 
-  // const Marker = ({ text, id, result }) => {
-  //   const [isHovered, setIsHovered] = useState(false);
-  //   // const singleCategory = actions.processCategory(result.category)[0];
-
-  //   const categories = actions.processCategory(result.category);
-  //   const icons = categories.map((category) =>
-  //     actions.getIconForCategory(category)
-  //   );
-
-  //   // const iconClass = actions.getIconForCategory(singleCategory);
-  //   const color = actions.getColorForCategory(result.category).color;
-  //   return (
-  //     <div
-  //       className="marker"
-  //       style={{ cursor: "pointer" }}
-  //       onMouseEnter={() => setIsHovered(true)}
-  //       onMouseLeave={() => setIsHovered(false)}
-  //       onClick={() => openModal(result)}
-  //     >
-  //       <div className="marker-icon">
-  //         <i className={iconClass} style={{ color: color }}></i>{" "}
-  //         {isHovered && text && <span className="marker-text">{text}</span>}
-  //       </div>
-  //     </div>
-  //   );
-  // };
-
   return (
-    <div className="map-container" style={{ height: "90vh", width: "90vw" }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: apiKey }}
-        center={city.center}
-        bounds={city.bounds}
-        defaultZoom={11}
-        onChange={(e) => handleBoundsChange(e)}
-        options={createMapOptions}
-      >
-        {store.boundaryResults.map((result, i) => (
-          <Marker
-            lat={result.latitude}
-            lng={result.longitude}
-            text={result.name}
-            key={i}
-            id={result.id}
-            openModal={openModal}
-            result={result}
+    <div className="map-frame">
+      <div className="step-1">
+        <div className="group-div">
+          <div className="step1-text">
+            <div className="step-text">
+              <p>Hit this button </p>
+              <i className="fa-solid fa-arrow-right-long"></i>
+            </div>
+            <div className="step-text">
+              <p>Or enter your zip code </p>
+              <i className="fa-solid fa-arrow-right"></i>
+            </div>
+          </div>
+          <MapSettings
+            geoFindMe={geoFindMe}
+            handleZipInputChange={handleZipInputChange}
+            zipInput={zipInput}
           />
-        ))}
-      </GoogleMapReact>
+        </div>
+        <div className="side-car">
+          {store.CATEGORY_OPTIONS &&
+          store.DAY_OPTIONS &&
+          store.GROUP_OPTIONS &&
+          categories &&
+          days &&
+          groups ? (
+            <ErrorBoundary>
+              <Selection
+                categories={categories}
+                setCategories={setCategories}
+                groups={groups}
+                setGroups={setGroups}
+                days={days}
+                setDays={setDays}
+                searchingToday={searchingToday}
+                setSearchingToday={setSearchingToday}
+                INITIAL_DAY_STATE={INITIAL_DAY_STATE}
+              />
+            </ErrorBoundary>
+          ) : (
+            message2Open && <p>Loading selection options...</p>
+          )}
+        </div>
+        {store.boundaryResults[0] && (
+          <div className="scroll-headers">
+            <Report />
+          </div>
+        )}
+      </div>
+      <div className="map-container" style={{ height: "80vh", width: "60vw" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: apiKey }}
+          center={city.center}
+          bounds={city.bounds}
+          defaultZoom={11}
+          onChange={(e) => handleBoundsChange(e)}
+          options={createMapOptions}
+        >
+          {store.boundaryResults.map((result, i) => (
+            <Marker
+              lat={result.latitude}
+              lng={result.longitude}
+              text={result.name}
+              key={i}
+              id={result.id}
+              openModal={openModal}
+              result={result}
+            />
+          ))}
+        </GoogleMapReact>
+      </div>
     </div>
   );
 };
