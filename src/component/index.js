5b76fdaf606a4104bc0cb8df58aa5b76dce6547f
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2023-11-25 14:25:19 -0600

    saving

--- src/component/index.js
+++ src/component/index.js
@@ -1,9 +1,7 @@
-export { default as SimpleMap } from './SimpleMap';
-export { default as Selection } from './Selection';
-export { default as Loading } from './Loading';
-export { default as ResourceCard } from './ResourceCard';
-export { default as MapSettings } from './MapSettings';
-export { default as Modal } from './Modal';
-export { default as Login } from './Login';
-export { default as LoginModal } from './LoginModal';
-
+export { default as SimpleMap } from "./SimpleMap";
+export { default as Selection } from "./Selection";
+export { default as ResourceCard } from "./ResourceCard";
+export { default as MapSettings } from "./MapSettings";
+export { default as Modal } from "./Modal";
+export { default as Login } from "./Login";
+export { default as LoginModal } from "./LoginModal";
