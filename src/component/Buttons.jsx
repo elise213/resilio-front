caac16966d056a215485348ad1681b5c59d02eb4
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2023-12-06 23:18:40 -0600

    saving

--- src/component/Buttons.jsx
+++ src/component/Buttons.jsx
@@ -20,29 +20,37 @@ const Buttons = ({
 }) => {
   return (
     <>
-      <div className="function-buttons">
-        <Button
-          className="customButton"
-          variant="contained"
-          startIcon={<i className="material-symbols-outlined">content_copy</i>}
-          ref={toggleDeckButtonRef}
-          onClick={() => toggleCardDeck()}
-        >
-          List
-        </Button>
+      <div>
+        <div className="function-buttons">
+          <Button
+            className="customButton"
+            variant="contained"
+            startIcon={
+              <i className="material-symbols-outlined">content_copy</i>
+            }
+            ref={toggleDeckButtonRef}
+            onClick={() => toggleCardDeck()}
+          >
+            List
+          </Button>
 
-        <Button
-          className="customButton"
-          variant="contained"
-          startIcon={<i className="material-symbols-outlined">filter_list</i>}
-          ref={toggleToolButtonRef}
-          onClick={() => {
-            setIsToolBoxOpen(!isToolBoxOpen);
-            console.log(isToolBoxOpen);
-          }}
-        >
-          Filters
-        </Button>
+          <Button
+            className="customButton"
+            variant="contained"
+            startIcon={<i className="material-symbols-outlined">filter_list</i>}
+            ref={toggleToolButtonRef}
+            onClick={() => {
+              setIsToolBoxOpen(!isToolBoxOpen);
+              console.log(isToolBoxOpen);
+            }}
+          >
+            Filters
+          </Button>
+        </div>
+        <p className={`the-plan`} onClick={() => setBackSide(!backSide)}>
+          <span className="material-symbols-outlined">arrow_forward</span>
+          {backSide ? "The Map" : "The Plan"}
+        </p>
       </div>
     </>
   );
