438a32ed72dd4d28440c172e3245895d4da788bb
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2024-03-17 14:40:11 -0600

    saving

--- src/component/HoverCard.jsx
+++ src/component/HoverCard.jsx
@@ -3,7 +3,10 @@ import Styles from "../styles/hoverCard.css";
 
 const HoverCard = (props) => {
   return (
-    <div className="hover-card" onClick={() => props.openModal(props.item)}>
+    <div
+      className="hover-card-inside"
+      onClick={() => props.openModal(props.item)}
+    >
       <p className="hover-title">{props.item.name}</p>
     </div>
   );
