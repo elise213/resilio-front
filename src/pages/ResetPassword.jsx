565674356652e8c1958c92cf8ea28440fdf6573e
Mara <maracodes213@gmail.com>
2024-08-02 23:57:58 -0500

    tab buttons on sidebar

---
+++ src/pages/ResetPassword.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+class ResetPassword extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleResetPassword = this.handleResetPassword.bind(this);
+  }
+
+  handleResetPassword(e) {
+    e.preventDefault();
+
+    var myHeaders = new Headers();
+    myHeaders.append(
+      "Authorization",
+      `Bearer ${sessionStorage.getItem("token")}`
+    );
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      changePassword: e.currentTarget.newPassword.value,
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("/change-changePassword", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        // Redirect to login page after password change
+        window.location.href = "/login";
+      })
+      .catch((error) => console.log("error", error));
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleResetPassword}>
+        <div>
+          <label htmlFor="newPassword">New Password:</label>
+          <input type="password" id="newPassword" name="newPassword" required />
+        </div>
+        <button type="submit">Reset Password</button>
+      </form>
+    );
+  }
+}
+
+export default ResetPassword;
